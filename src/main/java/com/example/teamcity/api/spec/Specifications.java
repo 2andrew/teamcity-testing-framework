351a42872c33e64a52e5adbf55db30bb3d4547b2
Andrii Shynkarov <>
2025-03-23 19:00:03 +0100

    feat: add swagger coverage calculation

--- src/main/java/com/example/teamcity/api/spec/Specifications.java
+++ src/main/java/com/example/teamcity/api/spec/Specifications.java
@@ -2,13 +2,18 @@ package com.example.teamcity.api.spec;
 
 import com.example.teamcity.api.config.Config;
 import com.example.teamcity.api.models.User;
+import com.github.viclovsky.swagger.coverage.FileSystemOutputWriter;
+import com.github.viclovsky.swagger.coverage.SwaggerCoverageRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
+import java.nio.file.Paths;
 import java.util.List;
+
+import static com.github.viclovsky.swagger.coverage.SwaggerCoverageConstants.OUTPUT_DIRECTORY;
 
 
 public class Specifications {
@@ -19,7 +24,11 @@ public class Specifications {
         reqBuilder.setBaseUri("http://" + Config.getProperty("host"));
         reqBuilder.setContentType(ContentType.JSON);
         reqBuilder.setAccept(ContentType.JSON);
-        reqBuilder.addFilters(List.of(new RequestLoggingFilter(), new ResponseLoggingFilter()));
+        reqBuilder.addFilters(List.of(new RequestLoggingFilter(), new ResponseLoggingFilter(), new SwaggerCoverageRestAssured(
+                new FileSystemOutputWriter(
+                        Paths.get("target/" + OUTPUT_DIRECTORY)
+                )
+        )));
         return reqBuilder;
     }
 
