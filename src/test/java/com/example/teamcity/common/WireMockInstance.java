11b7e9e10a5a624b0a59c69ccd2d0734eed0ffa6
Andrii Shynkarov <>
2025-03-24 02:17:19 +0100

    feat: debug wiremock

--- src/test/java/com/example/teamcity/common/WireMockInstance.java
+++ src/test/java/com/example/teamcity/common/WireMockInstance.java
@@ -5,6 +5,7 @@ import com.example.teamcity.api.models.BaseModel;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.MappingBuilder;
+import io.qameta.allure.Allure;
 import lombok.SneakyThrows;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
@@ -15,7 +16,7 @@ import static com.github.tomakehurst.wiremock.common.ContentTypes.CONTENT_TYPE;
 public final class WireMockInstance {
     private static final int PORT = Integer.parseInt(Config.getProperty("wiremockPort"));
 
-    private static WireMockServer wireMockServer;
+    private static WireMockServer wireMockServer = null;
 
     private WireMockInstance() {
     }
@@ -27,6 +28,15 @@ public final class WireMockInstance {
             configureFor("localhost", PORT);
             configureFor("http://" + Config.getProperty("host").split(":")[0], PORT);
             wireMockServer.start();
+
+            int retries = 5;
+            while (!wireMockServer.isRunning() && retries > 0) {
+                Thread.sleep(1000); // Wait 100ms
+                retries--;
+            }
+
+            Allure.addAttachment("WireMock Configuration1", "WireMock configured to use port: " + PORT);
+            Allure.addAttachment("WireMock Configuration2", "WireMock server started on port: " + wireMockServer.port() + ", running: " + wireMockServer.isRunning());
         }
 
         var jsonModel = new ObjectMapper().writeValueAsString(model);
